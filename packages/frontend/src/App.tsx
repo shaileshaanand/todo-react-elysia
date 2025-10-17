b262e50db183093b769cd0db7cf2f2f24fd05f8f
Shailesh Aanand <anaandshailu@gmail.com>
2025-10-18 03:56:38 +0530

    add tansrack router

--- packages/frontend/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>Hi</div>;
-}
-
-export default App;
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,11 +1,23 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 // biome-ignore lint/style/noNonNullAssertion: boilerplate
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
